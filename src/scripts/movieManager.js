de8fa16b05567806ad6f06186527b3c99a7f03b6
Andrew Green <green.andrew.b@gmail.com>
2021-06-30 13:58:52 -0500

    test

--- src/scripts/movieManager.js
+++ src/scripts/movieManager.js
@@ -1,16 +1,18 @@
 const movieFactory = (movie) => {
-    return `
+  return `
             <div class="movie">
             <h3 class="title">Title: ${movie.title}</h3>
             <h4 class="releaseDate">Release date: ${movie.releaseDate}</h4>
+            <h4 class="releaseDate">Release date: ${movie.releaseDate}</h4>
+            <h4 class="releaseDate">Release date: ${movie.id}</h4>
             </div>
             `;
 };
 
 const renderMovies = (movies) => {
-    const container = document.querySelector("#movie-container");
-    movies.forEach(movie => {
-        const movieAsHTML = movieFactory(movie)
-        container.innerHTML += movieAsHTML
-    });
+  const container = document.querySelector("#movie-container");
+  movies.forEach((movie) => {
+    const movieAsHTML = movieFactory(movie);
+    container.innerHTML += movieAsHTML;
+  });
 };
